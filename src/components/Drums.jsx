7bcef82bdf827bb945451221dcdab9c856f6e196
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-10-05 01:51:45 -0700

    keyboard controls

--- src/components/Drums.jsx
+++ src/components/Drums.jsx
@@ -1,15 +1,24 @@
+import { useEffect } from 'react'
 import drumsData from '../drums.json'
 
 function Drums() {
+    useEffect(() => {
+        window.addEventListener('keypress', e => {
+            const foundDrum = drumsData.find(drum => drum.key === e.key)
+            foundDrum && handlePlay(foundDrum.src)
+        })
+    }, [])
     function handlePlay(src){
         new Audio(src).play()
     }
+
   return (
     <section className="drums">
         {
             drumsData.map(drum => (
-                <button onClick={() => handlePlay(drum.src)} className={`drum-key ${drum.name.toLowerCase()}`} key={drum.name}>
+                <button onClick={() => handlePlay(drum.src)} className={`pad ${drum.name.toLowerCase()}`} key={drum.name}>
                     {drum.name}
+                    <span className='key'>{drum.key}</span>
                 </button>
             ))
         }
